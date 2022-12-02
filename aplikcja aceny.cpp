4920f3387a32862334cfea4e6a8a952bb0cc736d
mgh <114669510+mikigiush3k@users.noreply.github.com>
2022-12-02 19:12:56 +0100

    Add files via upload

---
+++ aplikcja aceny.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ocena;
+    cout<<"Jaka masz ocene ";
+    cin>>ocena;
+
+    switch(ocena)
+    {
+     case 1:
+            cout<<"Zla ocena";
+            break;
+     case 2:
+            cout<<"Ocena dla ludzi bez ambicji";
+            break;
+     case 3:
+            cout<<" przecietniętniak";
+            break;
+     case 4:
+            cout<<"Top g ocena";
+            break;
+     case 5:
+            cout<<"GIGACHAD ocena";
+            break;
+    }
+    system("PAUSE");
+    return 0;
+
+}
